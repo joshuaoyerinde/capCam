f7ea6d1a7678fb04fdbcd84861eecb78c410ee8a
Oyerinde Joshua <48280734+joshuaoyerinde@users.noreply.github.com>
2021-10-12 16:59:29 +0100

    first commit

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {}
-
+  constructor(public photoService: PhotoService) {}
+  snapPhotoToGallery(){
+    this.photoService.addNewToGallery();
+  }
+  ngOnInit():void{
+      // alert("am here ooo");
+  }
 }
